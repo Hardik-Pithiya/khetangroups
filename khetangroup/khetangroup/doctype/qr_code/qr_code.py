688ee4db9a24fe8087a7bb48e87a070a6999c4f8
rutikarathod1308 <rutikasanskartechnolab@gmail.com>
2024-01-23 11:59:23 +0530

    Print Format Changes And Qrcode File Add

--- khetangroup/khetangroup/doctype/qr_code/qr_code.py
+++ khetangroup/khetangroup/doctype/qr_code/qr_code.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2023, khetangroup and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from khetangroup.khetangroup.qr_demo import get_qr_code
 class QrCode(Document):
-	pass
+	def validate(self):
+		self.qr_data = get_qr_code({"Product Name":self.item_code ,"MRP":self.mrp,"Manufacturing Date":self.manufacturing_date ,"Size":self.size,"Color":self.color})
