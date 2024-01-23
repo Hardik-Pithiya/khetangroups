943d2e91255d8c70afb5b00c57b62970df639978
rutikarathod1308 <rutikasanskartechnolab@gmail.com>
2024-01-23 17:31:29 +0530

    qrcode file remove in git

--- khetangroup/khetangroup/doctype/qr_code/qr_code.py
+++ khetangroup/khetangroup/doctype/qr_code/qr_code.py
@@ -1,9 +1,4 @@
 # Copyright (c) 2023, khetangroup and contributors
 # For license information, please see license.txt
 
-import frappe
-from frappe.model.document import Document
-from khetangroup.khetangroup.qr_demo import get_qr_code
-class QrCode(Document):
-	def validate(self):
-		self.qr_data = get_qr_code({"Product Name":self.item_code ,"MRP":self.mrp,"Manufacturing Date":self.manufacturing_date ,"Size":self.size,"Color":self.color})
+# 
